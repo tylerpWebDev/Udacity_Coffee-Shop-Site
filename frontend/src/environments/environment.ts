cbff9a1a13f31d0789ba664ef53aeb3ebc237670
Tyler Proctor <tproctor4414@hotmail.com>
2020-07-13 11:47:17 -0700

    Setting up frontend and configuring auth connections.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,14 +1,24 @@
 /* @TODO replace with your variables
  * ensure all variables on this page match your project
- */
+ * export const environment = {
+ *  production: false,
+ *  apiServerUrl: 'http://127.0.0.1:5000/', // the running FLASK api server url
+ *  auth0: {
+ *    url: 'https://typcoffeeshop.us.auth0.com/', // the auth0 domain prefix
+ *    audience: 'coffeeshop', // the audience set for the auth0 app
+ *    clientId: '1dvl0GVfWwQP2syaydUyCpI3uvybH1s7', // the client id generated for the auth0 app
+ *    callbackURL: 'https://localhost:8100/', // the base url of the running ionic application. 
+ *  }
+ *};
+*/
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: 'http://127.0.0.1:5000/',
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'https://typcoffeeshop.us.auth0.com/',
+    audience: 'coffeeshop',
+    clientId: '1dvl0GVfWwQP2syaydUyCpI3uvybH1s7',
+    callbackURL: 'https://localhost:8100/'
   }
 };
