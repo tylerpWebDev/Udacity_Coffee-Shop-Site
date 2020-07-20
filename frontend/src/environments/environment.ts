d2dd402177f7b33c57d10ca36b3ac43a3992813e
Tyler Proctor <tproctor4414@hotmail.com>
2020-07-20 12:45:32 -0700

    Troubleshooting frontend.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -14,11 +14,13 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000/',
+  apiServerUrl: '127.0.0.1:5000',
   auth0: {
-    url: 'https://typcoffeeshop.us.auth0.com/',
+    url: 'typcoffeeshop.us',
     audience: 'coffeeshop',
     clientId: '1dvl0GVfWwQP2syaydUyCpI3uvybH1s7',
-    callbackURL: 'https://localhost:8100/'
+    callbackURL: 'https://localhost:8100'
   }
 };
+
+
